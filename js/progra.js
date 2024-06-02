db39211567600e56318cb7cfefd8de1bc2208235
henery <ernesto_1296@outlook.com>
2024-06-01 18:02:27 -0600

    jsScroll

--- js/progra.js
+++ js/progra.js
@@ -8,3 +8,13 @@ function hideMenu() {
     var menu = document.getElementById("menu");
     menu.classList.add("hidden");
 }
+// JavaScript para el desplazamiento suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
